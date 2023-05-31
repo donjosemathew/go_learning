eb6c75e606c42e41651bb43af6ba676fe58b961e
dontech09 <donnjondanmakal@gmail.com>
2023-05-31 19:14:06 +0530

    Go text formatting

--- main.go
+++ main.go
@@ -1,6 +1,23 @@
 package main
-
-import "fmt"
 func main(){
-	fmt.PrintLn("ds")
+	// fmt.Println("ds")
+
+	// //string
+	// var num string="Robert"
+	// num1:="Masala"
+	// fmt.Println(num,num1)
+	// //Numbers
+	// var sum int8=6
+	// fmt.Println(sum)
+	// //float
+	// var num3 float32=34.5
+	// fmt.Println(num3)
+
+	//string formatting
+	// a:=23
+	// b:="India is"
+	// fmt.Printf("India is my country %v and %v",a,b)
+	// fmt.Printf("India is my country %T and %T",a,b)
+	// var c=fmt.Sprintf("India is my country %T and %T",a,b)
+	// fmt.Println(c)
 }
