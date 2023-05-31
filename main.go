38a7b32367acce7774bcd1b54ab976a17ab5b45a
dontech09 <donnjondanmakal@gmail.com>
2023-05-31 19:36:30 +0530

    Slice/Array

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
-func main(){
+
+func main() {
 	// fmt.Println("ds")
 
 	// //string
@@ -20,4 +21,14 @@ func main(){
 	// fmt.Printf("India is my country %T and %T",a,b)
 	// var c=fmt.Sprintf("India is my country %T and %T",a,b)
 	// fmt.Println(c)
+
+	// //Arrays
+	// var num = [4]int{2, 3, 4}
+	// fmt.Println(num)
+	// //slice
+	// var num2 = []int{2, 3, 4}
+	// num2=append(num2, 23)
+	// fmt.Println(num2)
+	// num3:=num[1:2]
+	// fmt.Println(num3)
 }
